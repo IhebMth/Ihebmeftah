3d9677a1e591f68370b8ec15eba1988ad1df65da
IhebMth <89525182+IhebMth@users.noreply.github.com>
2022-10-02 20:42:36 +0100

    Update style-switcher.js

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -89,4 +89,11 @@ links.forEach((link, index) =>
         });
     })
     
+    const navLinks = document.querySelector("nav").querySelectorAll("a")
+navLinks.forEach(link => {
+link.addEventListener("click", function () {
+    navLinks.forEach(nav=> nav.classList.remove("active"))
+    this.classList.add("active")
+})
+})
 */
